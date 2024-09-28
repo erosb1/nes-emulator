ea841b19b3b924eb920a9ffa3e39bca0428a2d3b
Elias Rosberg <elias.rosberg@hotmail.com>
2024-09-28 21:08:38 +0200

    Add a bunch of functions to SDLInstance
    sdl_clear_screen - clears the pixel buffer
    sdl_put_pixel - set one pixel in the pixel buffer
    sdl_draw_fram - draws the pixel buffer to the window, using the GPU
    sdl_poll_events - polls the window for events, returns an integer with bits set for each event type

--- sdl/sdl-instance.c
+++ sdl/sdl-instance.c
@@ -1,54 +1,99 @@
 
 #include "sdl-instance.h"
 
-int sdl_instance_init(SDLInstance *instance) {
+int sdl_instance_init(SDLInstance *sdl_instance) {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
         return -1;
     }
 
-    instance->window = SDL_CreateWindow(SDL_WINDOW_TITLE, SDL_WINDOWPOS_UNDEFINED,
+    sdl_instance->window = SDL_CreateWindow(SDL_WINDOW_TITLE, SDL_WINDOWPOS_UNDEFINED,
                                       SDL_WINDOWPOS_UNDEFINED, SDL_WINDOW_WIDTH,
                                       SDL_WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
-    if (instance->window == NULL) {
+    if (sdl_instance->window == NULL) {
         printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
         return -1;
     }
 
-    instance->surface = SDL_GetWindowSurface(instance->window);
+    sdl_instance->renderer = SDL_CreateRenderer(sdl_instance->window, -1, SDL_RENDERER_ACCELERATED);
+    if (!sdl_instance->renderer) {
+        printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(sdl_instance->window);
+        return -1;
+    }
 
-    instance->title = SDL_WINDOW_TITLE;
-    instance->width = SDL_WINDOW_WIDTH;
-    instance->height = SDL_WINDOW_HEIGHT;
+    sdl_instance->texture = SDL_CreateTexture(sdl_instance->renderer, SDL_PIXELFORMAT_ARGB8888,
+                            SDL_TEXTUREACCESS_STREAMING, SDL_WINDOW_WIDTH, SDL_WINDOW_HEIGHT);
 
-    SDL_UpdateWindowSurface(instance->window);
+    sdl_instance->title = SDL_WINDOW_TITLE;
+    sdl_instance->pixel_buffer = (uint32_t *)malloc(SDL_WINDOW_WIDTH * SDL_WINDOW_HEIGHT * sizeof(Uint32));
+    sdl_instance->width = SDL_WINDOW_WIDTH;
+    sdl_instance->height = SDL_WINDOW_HEIGHT;
 
     return 0;
 }
 
-void sdl_event_loop(SDLInstance *instance) {
-    int quit = 0;
-    SDL_Event e;
-
-    // Event loop
-    while (!quit) {
-        // Handle events on the queue
-        while (SDL_PollEvent(&e) != 0) {
-            // User requests quit
-            if (e.type == SDL_QUIT) {
-                quit = 1;
-            }
-        }
-
-        // Optionally, you can clear and update the surface in each loop iteration
-        SDL_UpdateWindowSurface(instance->window);
-
-        // Add a small delay to avoid overloading the CPU
-        SDL_Delay(16); // Roughly 60 FPS
+void sdl_clear_screen(SDLInstance *sdl_instance) {
+    for (int i = 0; i < sdl_instance->width * sdl_instance->height; i++) {
+        sdl_instance->pixel_buffer[i] = 0x00000000;
     }
 }
 
-void sdl_instance_destroy(SDLInstance *instance) {
-    SDL_DestroyWindow(instance->window);
+void sdl_put_pixel(SDLInstance *sdl_instance, int x, int y, uint32_t color) {
+    if (x >= 0 && x < sdl_instance->width && y >= 0 && y < sdl_instance->height) {
+        sdl_instance->pixel_buffer[y * sdl_instance->width + x] = color;
+    }
+}
+
+void sdl_draw_frame(SDLInstance *sdl_instance) {
+    SDL_UpdateTexture(sdl_instance->texture, NULL, sdl_instance->pixel_buffer, sdl_instance->width * sizeof(Uint32));
+    SDL_RenderClear(sdl_instance->renderer);
+    SDL_RenderCopy(sdl_instance->renderer, sdl_instance->texture, NULL, NULL);
+    SDL_RenderPresent(sdl_instance->renderer);
+}
+
+uint32_t sdl_poll_events() {
+    SDL_Event event;
+    uint32_t event_mask = NONE;
+
+    while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT) {
+            event_mask |= WINDOW_QUIT;
+        }
+        const Uint8 *state = SDL_GetKeyboardState(NULL);
+        if (state[SDL_SCANCODE_X]) {
+            event_mask |= NES_A_BUTTON;
+        }
+        if (state[SDL_SCANCODE_Z]) {
+            event_mask |= NES_B_BUTTON;
+        }
+        if (state[SDL_SCANCODE_RSHIFT]) {
+            event_mask |= NES_SELECT_BUTTON;
+        }
+        if (state[SDL_SCANCODE_RETURN]) {
+            event_mask |= NES_START_BUTTON;
+        }
+        if (state[SDL_SCANCODE_UP]) {
+            event_mask |= NES_DPAD_UP;
+        }
+        if (state[SDL_SCANCODE_DOWN]) {
+            event_mask |= NES_DPAD_DOWN;
+        }
+        if (state[SDL_SCANCODE_LEFT]) {
+            event_mask |= NES_DPAD_LEFT;
+        }
+        if (state[SDL_SCANCODE_RIGHT]) {
+            event_mask |= NES_DPAD_RIGHT;
+        }
+    }
+
+    return event_mask;
+}
+
+void sdl_instance_destroy(SDLInstance *sdl_instance) {
+    if (sdl_instance->pixel_buffer) free(sdl_instance->pixel_buffer);
+    if (sdl_instance->texture) SDL_DestroyTexture(sdl_instance->texture);
+    if (sdl_instance->renderer) SDL_DestroyRenderer(sdl_instance->renderer);
+    if (sdl_instance->window) SDL_DestroyWindow(sdl_instance->window);
     SDL_Quit();
 }
--- sdl/sdl-instance.h
+++ sdl/sdl-instance.h
@@ -2,21 +2,51 @@
 #define SDL_INSTANCE_H
 
 #include <SDL2/SDL.h>
+#include <stdint.h>
+#include <stdio.h>
 
 #define SDL_WINDOW_WIDTH  800
 #define SDL_WINDOW_HEIGHT 600
 #define SDL_WINDOW_TITLE  "NES Emulator"
 
+// NES Controller to keyboard mapping
+//#define NES_A_BUTTON      SDL_SCANCODE_X
+//#define NES_B_BUTTON      SDL_SCANCODE_Z
+//#define NES_SELECT_BUTTON SDL_SCANCODE_RSHIFT
+//#define NES_START_BUTTON  SDL_SCANCODE_RETURN
+//#define NES_DPAD_UP       SDL_SCANCODE_UP
+//#define NES_DPAD_DOWN     SDL_SCANCODE_DOWN
+//#define NES_DPAD_LEFT     SDL_SCANCODE_LEFT
+//#define NES_DPAD_RIGHT    SDL_SCANCODE_RIGHT
+
+typedef enum SDLEmulatorEvent {
+    NONE              = 0,
+    WINDOW_QUIT       = 1 << 0,
+    NES_A_BUTTON      = 1 << 1,
+    NES_B_BUTTON      = 1 << 2,
+    NES_SELECT_BUTTON = 1 << 3,
+    NES_START_BUTTON  = 1 << 4,
+    NES_DPAD_UP       = 1 << 5,
+    NES_DPAD_DOWN     = 1 << 6,
+    NES_DPAD_LEFT     = 1 << 7,
+    NES_DPAD_RIGHT    = 1 << 8,
+} SDLEmulatorEvent;
+
 typedef struct SDLInstance {
     SDL_Window *window;
-    SDL_Surface *surface;
+    SDL_Renderer *renderer;
+    SDL_Texture *texture;
+    uint32_t *pixel_buffer;
     int width;
     int height;
     const char *title;
 } SDLInstance;
 
-int sdl_instance_init(SDLInstance *instance);
-void sdl_event_loop(SDLInstance *instance);
-void sdl_instance_destroy(SDLInstance *instance);
+int sdl_instance_init(SDLInstance *sdl_instance);
+void sdl_clear_screen(SDLInstance *sdl_instance);
+void sdl_put_pixel(SDLInstance *sdl_instance, int x, int y, uint32_t color);
+void sdl_draw_frame(SDLInstance *sdl_instance);
+uint32_t sdl_poll_events(SDLInstance *sdl_instance);
+void sdl_instance_destroy(SDLInstance *sdl_instance);
 
 #endif
