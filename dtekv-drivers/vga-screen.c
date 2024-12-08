fce899b7baf1785be49fa402d35f0d610588fe49
Aws Majed <awsm@kth.se>
2024-12-08 17:35:59 +0100

    Switch vga_screen_clear to memset

--- dtekv-drivers/vga-screen.c
+++ dtekv-drivers/vga-screen.c
@@ -9,10 +9,7 @@
 volatile uint8_t *VGA = (volatile uint8_t *)0x08000000;
 volatile uint32_t *VGA_CTRL = (volatile uint32_t *)0x04000100;
 
-void vga_screen_clear() {
-    for (uint32_t i = 0; i < VGA_SCREEN_WIDTH * VGA_SCREEN_HEIGHT; i++)
-        VGA[i] = 0x00;
-}
+void vga_screen_clear() { memset(VGA, 0x00, VGA_SCREEN_WIDTH * VGA_SCREEN_HEIGHT); }
 
 void vga_screen_put_pixel(uint32_t x, uint32_t y, uint8_t c) {
     if (x >= VGA_SCREEN_WIDTH || y >= VGA_SCREEN_HEIGHT)
